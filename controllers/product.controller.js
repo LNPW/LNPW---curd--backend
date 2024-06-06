1a65d44d95c5ba7453f05acec1b4565f137ec1e5
LNPW <lnpw786@gmail.com>
2024-06-06 00:49:30 -0700

    first commit

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,26 +3,26 @@ const Product= require('../models/product.model')
 
 const getProducts = async(req,res)=>{
     const product = await Product.find({})
-    res.json (product)
+    return res.status(200).json({"message":"get all Product","status":"true"})
 } 
 const getProduct =async(req,res)=>{
     const {id}= req.params
     const product = await Product.findById(id)
-    res.json (product)
+    return res.status(200).json({"message":"Product","status":"message.id"})
 }
 const createProduct =async(req,res)=>{
     const product = await Product.create(req.body)
-        res.json (product)
+    return res.status(200).json({"message":"Product added","status":"message.id"})
 }
 
 const updateProduct =async(req,res)=>{
     const {id}= req.params
     const product = await Product.findByIdAndUpdate(id , req.body)
     if (!product){
-        res.json("Product not found")
+        return res.status(400).json({"message":"Product not found","status":false})
     }
      const UpdatedProduct= await Product.findById(id)
-    res.json(UpdatedProduct)
+     return res.status(200).json({"message":"Product updated","status":true})
 }
 const deleteProduct =async(req,res)=>{
     const {id}= req.params
